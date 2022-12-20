b3cbf29b276b5a530d72836167c0b7c2ec572655
Alwyn A <alwynsimiyu@gmail.comgit config --global user.name Alwyn>
2022-12-20 12:35:36 +0300

    Modify html & Css, Fix all linter issues

--- myscript.js
+++ myscript.js
@@ -1,20 +1,18 @@
-const hamburger = document.querySelector(".hamburger");
-const navbar = document.querySelector(".navbar");
-const closeBtn = document.querySelector(".close-btn");
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.querySelector('.navbar');
+const closeBtn = document.querySelector('.close-btn');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navbar.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navbar.classList.toggle('active');
 });
 
-closeBtn.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navbar.classList.toggle("active");
+closeBtn.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navbar.classList.toggle('active');
 });
 
-document.querySelectorAll(".nav-icons").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navbar.classList.remove("active");
-  })
-);
+document.querySelectorAll('.nav-icons').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navbar.classList.remove('active');
+}));
